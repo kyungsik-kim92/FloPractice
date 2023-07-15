137560009e8f36f9efb661b93dec80554ca6e94b
Kim Kyungsik <ksiki1@naver.com>
2023-07-15 22:04:16 +0900

    탭 레이아웃 뷰페이저 실습

---
+++ app/src/main/java/com/example/flopractice/SongFragment.kt
@@ -0,0 +1,23 @@
+package com.example.flopractice
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.flopractice.databinding.FragmentSongBinding
+
+class SongFragment :Fragment() {
+    lateinit var binding : FragmentSongBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding= FragmentSongBinding.inflate(inflater, container,false)
+
+
+        return binding.root
+    }
+}
