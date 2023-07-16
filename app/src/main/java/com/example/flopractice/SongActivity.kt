6e1a589492db792075090ecf17c22363ddff3252
Kim Kyungsik <ksiki1@naver.com>
2023-07-16 23:27:47 +0900

    스레드 이용해서 타이머 적용

--- app/src/main/java/com/example/flopractice/SongActivity.kt
+++ app/src/main/java/com/example/flopractice/SongActivity.kt
@@ -1,6 +1,7 @@
 package com.example.flopractice
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.flopractice.databinding.ActivitySongBinding
@@ -9,12 +10,18 @@ class SongActivity : AppCompatActivity() {
 
 
     lateinit var binding: ActivitySongBinding
+    lateinit var song: Song
+    lateinit var timer: Timer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivitySongBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        initSong()
+        setPlayer(song)
 
 
         binding.songDownIb.setOnClickListener {
@@ -29,15 +36,47 @@ class SongActivity : AppCompatActivity() {
             setPlayerStatus(true)
         }
 
-        if (intent.hasExtra("title") && intent.hasExtra("singer")){
-            binding.songMusicTitleTv.text = intent.getStringExtra("title")
-            binding.songSingerNameTv.text = intent.getStringExtra("singer")
-        }
 
     }
 
+    private fun initSong() {
+        if (intent.hasExtra("title") && intent.hasExtra("singer")) {
+            song = Song(
+                intent.getStringExtra("title")!!,
+                intent.getStringExtra("singer")!!,
+                intent.getIntExtra("second", 0),
+                intent.getIntExtra("playTime", 0),
+                intent.getBooleanExtra("isPlaying", false)
+            )
+        }
+        startTimer()
+    }
 
-    fun setPlayerStatus(isPlaying: Boolean) {
+    override fun onDestroy() {
+        super.onDestroy()
+        timer.interrupt()
+    }
+
+    private fun setPlayer(song: Song) {
+        binding.songMusicTitleTv.text = intent.getStringExtra("title")
+        binding.songSingerNameTv.text = intent.getStringExtra("singer")
+        binding.songStartTimeTv.text =
+            String.format("%02d:%02d", song.second / 60, song.second % 60)
+
+        binding.songEndTimeTv.text =
+            String.format("%02d:%02d", song.second / 60, song.second % 60)
+
+        binding.songProgressSb.progress =
+            (song.second * 1000 / song.playTime)
+
+        setPlayerStatus(song.isPlaying)
+    }
+
+
+    private fun setPlayerStatus(isPlaying: Boolean) {
+        song.isPlaying = isPlaying
+        timer.isplaying = isPlaying
+
 
         if (isPlaying) {
             binding.songMiniplayerIv.visibility = View.VISIBLE
@@ -50,6 +89,57 @@ class SongActivity : AppCompatActivity() {
         }
 
 
+    }
+
+    private fun startTimer() {
+        timer = Timer(song.playTime, song.isPlaying)
+        timer.start()
+
+
+    }
+
+    inner class Timer(private val playTime: Int, var isplaying: Boolean = true) : Thread() {
+
+        private var second: Int = 0
+        private var mills: Float = 0f
+
+        override fun run() {
+            super.run()
+            try {
+                while (true) {
+                    if (second >= playTime) {
+                        break
+                    }
+
+                    if (isplaying) {
+                        sleep(50)
+                        mills += 50
+                        runOnUiThread {
+
+                            binding.songProgressSb.progress = ((mills / playTime) * 100).toInt()
+
+                        }
+                        if (mills % 1000 == 0f) {
+                            runOnUiThread {
+                                binding.songStartTimeTv.text =
+                                    String.format("%02d:%02d", second / 60, second % 60)
+                            }
+
+                            second++
+
+                        }
+
+                    }
+                }
+
+            } catch (e: InterruptedException) {
+
+                Log.d("Song","쓰레드가 죽었습니다${e.message}")
+
+            }
+
+
+        }
     }
 
 }
